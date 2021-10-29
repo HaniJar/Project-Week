a4cb10adadaf375d03cdc438c0ceb5e96cd9b193
HaniJar <91248569+HaniJar@users.noreply.github.com>
2021-10-29 23:02:55 +0200

    Add files via upload

--- testimonial.js
+++ testimonial.js
@@ -1,1 +1,12 @@
+$(document).ready(function() {
+    $('.user-upload-img').magnificPopup({
+      delegate: 'a', // child items selector, by clicking on it popup will open
+      type: 'image',
+        gallery: {
+        enabled: true
+        }
+      // other options
+    });
+    });
 
+    
